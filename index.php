bc07a55c0312eb5950980269d45e250eaffb8c6f
Habib Can Yılmaz <160408520+habibcandev@users.noreply.github.com>
2024-03-20 04:01:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>hacked by habibcan</title>
+    <meta charset="utf-8">
+    </head>
+    <body bgcolor="black" style="color:dark"><center>
+    <br><br><br>
+    <img src="https://media.discordapp.net/attachments/1060596435965448302/1219802614279962756/1024px-Anonymous_emblem.png?ex=660ca0b6&is=65fa2bb6&hm=d314b40fb117e9605070bb9d45f4e66736ced5f61b2124b000e9e8cd041c5058&=&format=webp&quality=lossless&width=607&height=607">
+    <br><br><br>
+    <font face="Courier New" color="gold" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong>hacked by habibcan - dayioglu</strong></span></font>
+    <br><br><br><br>
+    <font face="Courier New" color="red" size="3"> </font><font face="Courier New" color="white" size="3">Ifsacilik oynayan tayfaya selam olsun!<br><br> Cok seviyorsunuz herhalde<br>Discord : <a href="https://discord.gg/zzindex" style="color: #fff; text-decoration: underline; font-size: 16px;">Tıkla!</a>
+
+    <br><br>
+    <font face="Courier New" color="gold" size="5"><span style="text-shadow: 0px 0px 9px ;"><strong>t.me/zzindex - discord.gg/zzindex </strong></span></font>&nbsp;&nbsp;<font face="Courier New" color="red" size="5"><strong>Presented </strong></font><br><br><br>  <audio src="dexus.mp3" controls autoplay>
+    </center></font></font><script>(function(){var js = "window['__CF$cv$params']={r:'798e6c62da157246',m:'4yArd1XQkTibreqcAM.DeO0GNl4.p.a7x_nqPC30WsA-1676300319-0-AccKND6qUZs0uP8zXSkg9TNeGKmm17MEj0uVaf9chK6/8fPr5VisKqNlx/VcDeLoLdnEmxp240bOpqsY9EbPkJgy78wFa7YgbUVZhftBRhVR1ILbBAHcck9Ch7tXLrft0g==',s:[0x76fee701b4,0x297d5eca15],u:'/cdn-cgi/challenge-platform/h/b'};var now=Date.now()/1000,offset=14400,ts=''+(Math.floor(now)-Math.floor(now%offset)),_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/h/b/scripts/alpha/invisible.js?ts='+ts,document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.nonce = '';_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script></html>
